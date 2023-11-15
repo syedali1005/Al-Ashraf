1343e1a07210515f3cc652686f32bbe3add078a5
Syed Ali Ahmed <syedaliahmad22@gmail.com>
2023-11-15 12:34:23 +0500

    Added Babel File

--- src/components/LineGradient.jsx
+++ src/components/LineGradient.jsx
@@ -1,7 +1,5 @@
-
-const LineGradient = ({width = "w-full"}) => {
-    return <div className="h-0.5 ${width} bg-gradient-black" />;
-    
-}
-
-export default LineGradient;
+const LineGradient = ({ width = "w-full" }) => {
+    return <div className={`h-0.5 ${width} bg-gradient-black`} />;
+  };
+  
+  export default LineGradient;
